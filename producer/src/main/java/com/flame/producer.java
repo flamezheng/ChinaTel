b39b2135c5664153bea05376ea009fc8f53d82d0
flame <zhengtianji001@163.com>
2018-06-06 11:59:22 +0800

    # 第一次提交

---
+++ producer/src/main/java/com/flame/producer.java
@@ -0,0 +1,17 @@
+package com.flame;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 生成数据的工具类
+ */
+public class producer {
+
+    private List<String> phoneNumber = new ArrayList<String>();
+    private Map<String, String> phoneName = new HashMap<String, String>();
+
+
+}
